f5dd9b165efc6ced7adadbb7b7f00e857c6ce108
Kevin Escobar Miranda <info@EscobarMiranda.com>
2016-09-28 11:27:23 -0600

    Create tracking view

---
+++ app/components/tracking/tracking.controller.js
@@ -0,0 +1,20 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('app.tracking')
+    .controller('TrackingController', TrackingController);
+
+  /* @ngInject */
+  function TrackingController() {
+    var vm = this;
+
+    activate();
+
+    function activate() {
+
+    }
+
+  }
+
+})();
